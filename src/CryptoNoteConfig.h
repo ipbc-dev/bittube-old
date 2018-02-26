d2bb2fd08b3b4e5e0e64720298fdf03122f41d40
F.Harms <f.harms@devcups.com>
2018-02-26 16:18:33 +0000

    Revert "Change DIFFICULTY_WINDOW_V2, UPGRADE_HEIGHT_V2. Small CMAKE fixes."
    This reverts commit c559036e9117b8d4a02988861084e653ed865a94.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -51,7 +51,7 @@ const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(100000);
 const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // blocks
-const size_t   DIFFICULTY_WINDOW_V2                          = 70; // blocks (was 17)
+const size_t   DIFFICULTY_WINDOW_V2                          = 17; // blocks
 const size_t   DIFFICULTY_CUT                                = 60;  // timestamps to cut after sorting
 const size_t   DIFFICULTY_LAG                                = 15;  // !!!
 static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
@@ -64,16 +64,16 @@ const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
 
 const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;     //seconds, one day
-const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 2; //seconds, two days
-const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 2;  // CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL * CRYPTONOTE_MEMPOOL_TX_LIVETIME = time to forget tx
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7; //seconds, one week
+const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7;  // CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL * CRYPTONOTE_MEMPOOL_TX_LIVETIME = time to forget tx
 
 const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT * 30 / 100;
 const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
 const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
-const uint32_t UPGRADE_HEIGHT_V2                             = 23000;
+const uint32_t UPGRADE_HEIGHT_V2                             = 60000;
 const uint32_t UPGRADE_HEIGHT_V3                             = 4294967294;
-const unsigned UPGRADE_VOTING_THRESHOLD                      = 80;  // percent
+const unsigned UPGRADE_VOTING_THRESHOLD 		     = 80;               // percent
 const uint32_t   UPGRADE_VOTING_WINDOW                       = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t   UPGRADE_WINDOW                              = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "Bad UPGRADE_VOTING_THRESHOLD");
@@ -122,14 +122,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
 const char* const SEED_NODES[] = { 
   "seed1.ipbc.io:24181",
-  "seed2.ipbc.io:24181",
-  "seed3.ipbc.io:24181",
-  "seed4.ipbc.io:24181",
-  "seed5.ipbc.io:24181",
-  "seed6.ipbc.io:24181",
-  "seed7.ipbc.io:24181",
-  "seed8.ipbc.io:24181",
-  "seed9.ipbc.io:24181"
+  "seed2.ipbc.io:24181"
 };
 
 struct CheckpointData {
@@ -138,30 +131,12 @@ struct CheckpointData {
 };
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = { 
-  {50,   "dbfc2fce97b6c95bb130cb88c3e06a3020b144719f6660fb5d8a2b2bc847318f"},
-  {200,  "3b0d7cecca432c41ef84b6ab930e859274c117c0319e34c36fbc858f7fc6255c"},
-  {1000, "ab459e59b90d38f2cf370835dd5558bfd79f26f5034a58128e6aded3c611c594"},
-  {1500, "3c57b653c48f1ad113b7053aea628897b636ad4f366f7ee1d3f7aae018c07549"},
-  {1600, "3b5bc6181690ae31f0fae0426c02d6541af4a7d2dcc12e5c0ac53f81616e8dc1"},
-  {2000, "cc9537cae496f1dfed600af2238d617cf0c7c65801ad5672ec04580c4b2438ce"},
-  {5500, "53c1af87ee0d0607770e5911c0d755b882bc210bfaaf8d50091dfa32d17ac380"},
-  {5515, "dcdcd66f456c9a33510d473f6a1bc293225e597e0c0ec815a1c54f72f8aa8aef"},
-  {6166, "eedf1491ea2237082749e92b4c6dd3c11cfb7ef6cf52fea8c8620fc01a72e591"},
-  {7000, "62dafd54e4ef7e572b3b89617007a80194a224e12396e4291447ccecb5ca598c"},
-  {8000, "bd1b9b3a2e98e375fcfbb5358033adc448d347b5657a11c1c29eb7375a07f0b2"},
-  {9000, "b25d9800a515eabab17e5ab9fcbec38797cf61c7406be34795f607beccc14c26"},
-  {10000, "554e4fe4d7c60ac17ba4880713137973dc65a04199bdd91b363ba0a36d4b92c3"},
-  {11000, "e5e4ccd719fe7f29f6e85e3a965fe33bd165fe950a3c9a9c48830c25578d85b8"},
-  {12000, "0ecc1f453b184d7db6d3e3ac5ef5d41763d7e49d807420a768593a0ed0e466f5"},
-  {13000, "82c47518a8ced16e408958636bfde75c22703c5b624e36692466e2712818bd41"},
-  {14000, "c4ec39612d4a35b7e528b42ccc5575f021203874656c148fc36c62a46d65067d"},
-  {15000, "726fc5cfc1e48740b2c77575393fdc7db299a47cc9d90d40d088c22fe3b2d129"},
-  {16000, "af062ec6f9312e78f1f4824ac7059c6f58553684657c5e9cf5405dba820beaab"},
-  {17000, "95cac6667df0f6e4068c9f6551198de51c7ce6c886170334a47ebb4b440633c5"},
-  //{18000, "0ce592e6d0059f7661193d1439d90210db4809ca6a371c2f19f694fab30d171b"},
-  //{19000, "1b7be4a99458c1b1d75e5adbb05dc399b234f88dd0e0ba1ba99d6f52342a26bd"},
+
 };
   
 } // CryptoNote
 
 #define ALLOW_DEBUG_COMMANDS
+
+
+
