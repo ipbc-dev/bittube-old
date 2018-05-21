c3d662b7490ca585c9493827eb5b47238461aa38
Markus Behm <markus@ipbc.io>
2018-05-21 14:06:33 +0000

    Reverted BitTube base reward splitting to take all the tx fees

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -231,11 +231,8 @@ namespace CryptoNote {
 		uint64_t sharedReward = 0;
 		uint64_t minerReward = blockReward;
 		if (blockMajorVersion >= BLOCK_MAJOR_VERSION_4) {
-			uint64_t noFeeReward;
-			getBlockReward(blockMajorVersion, medianSize, currentBlockSize, alreadyGeneratedCoins, 0, noFeeReward, emissionChange, pre_mishap);
-			sharedReward = (noFeeReward * BITTUBE_SHARE_PERCENT / 100);
-			minerReward = noFeeReward - sharedReward;
-			sharedReward += blockReward - noFeeReward;
+			sharedReward = blockReward * BITTUBE_SHARE_PERCENT / 100;
+			minerReward = blockReward - sharedReward;
 			maxOuts -= 1; // save one out for shared transaction
 		}
 
